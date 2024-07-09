c8ddecf599cd606c4c6d9ec9d0ff83d9c6fd7110
Nora Alsultan <97395867+Norakhalsu@users.noreply.github.com>
2024-07-09 11:46:13 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,122 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+/* Write a Java program that accept three numbers from the user and print the largest number .*/
+
+     /*   Scanner scanner=new Scanner(System.in);
+        System.out.println(" write frist number ");
+        int n1=scanner.nextInt();
+        System.out.println(" enter second number ");
+        int n2 = scanner.nextInt();
+        System.out.println(" enter third number ");
+        int n3 =scanner.nextInt();
+
+        if ( n1 > n2 && n1 > n3 ){
+            System.out.println(" the large is "+ n1);
+        } else if (n2 > n1 && n2 > n3 ) {
+            System.out.println(" the large is "+ n2 );
+
+        } else System.out.println(" the large is "+ n3);  */
+
+
+
+
+/*2.Write a Java program that accept a String and a number
+from the user,then print the character in the given index .*/
+
+
+    /*  Scanner  scanner=new Scanner(System.in);
+        System.out.println(" write a string ");
+        String string=scanner.next();
+        System.out.println(" enter a number to index ");
+        int n= scanner.nextInt();
+
+        System.out.println(string.charAt(n));  */
+
+
+
+
+  /*3 Write a program to enter the numbers till the user wants and
+        at the end it should display the sum entered . */
+
+
+
+  /* int sum=0;
+  Scanner scanner = new Scanner(System.in);
+    while (true){
+    System.out.println(" enter number  // -1 to stop  ");
+    int n1=scanner.nextInt();
+
+    if (n1 == -1 ){
+        break;
+    }
+    sum = sum + n1 ;
+
+    }
+    System.out.println(" the sum of number is "+sum);   */
+
+
+
+
+
+ /* 4.Write a Java program to find positive and negative numbers of a given array:
+  Original Array: [10, -21 , 30, 31, -25]
+  Expected Output:
+     10  is a positive number
+     -21 is a negative number
+      30 is a positive number
+      31 is a positive number
+     -25 is a negative number    */
+
+
+      /*  int[] arr = new int[]{10, -21 , 30, 31, -25};
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] > 0 ){
+                System.out.println(arr[i]+ " posistive number ");
+            } else if (arr[i] < 0 ) {
+                System.out.println(arr[i]+" nigative number ");
+            }  else System.out.println(arr[i] + " the number Zero");
+        }     */
+
+
+
+
+
+
+/* 5. Write a Java program to find shortest word of a given array:
+- Original Array: [“Tuwaiq”, “Bootcamp” , “Student”,”JAVA”]
+    - Expected Output: JAVA    */
+
+
+
+/* String[] arr=new String[]{"Tuwaiq" ,"Bootcamp","Student","JAVA" } ;
+
+String m0 = arr[0].toString();
+String m1=arr[1].toString();
+String m2 =arr[2].toString();
+String m3=arr[3].toString();
+     int n0 = arr[0].length();
+     int n1 = arr[1].length();
+     int n2 = arr[2].length();
+     int n3 = arr[3].length();
+
+     if (n0 < n1 && n0 < n2 && n0 < n3) {
+         System.out.println(m0); }
+
+  else if (n1 < n0 && n1 < n2 && n1 < n3) {
+         System.out.println(m1);
+     }
+  else if (n2 < n0 && n2 < n1 && n2 < n3) {
+         System.out.println(m2);
+
+     } else System.out.println(m3);  */
+
+
+
+
+
+
+    }}
